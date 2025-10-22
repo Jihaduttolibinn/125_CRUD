993b5cea82465ef1b63480a950bf60eee66a93b2
jihaduttolibinn <jihaduttolibin02@gmail.com>
2025-10-22 11:43:29 +0700

    Menambahkan app.post

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`server running on port ${port}`);
 });
 
 const db = mysql.createConnection({
@@ -29,3 +29,28 @@ db.connect((err) => {
     console.log('Connected Sussessfully.');
 });
 
+app.get('/api/mahasiswa', (req, res) => {
+    db.query('SELECT * FROM biodata', (err, results) => {
+        if (err) {
+            console.error('Error executing query: ' + err.stack);
+            res.status(500).send('Error fetching mahasiswa');
+            return;
+        }
+        res.json(results);
+    });
+});
+
+app.post('/api/mahasiswa', (req, res) => {
+    const { nama, alamat, agama } = req.body;
+
+    if (!nama || !alamat || !agama) {
+        return res.status(400).json({ message : "nama, alamat, agama harus diisi"});
+    }
+
+    db.query('INSERT INTO biodata (nama, alamat, agama) VALUES (?, ?, ?)', 
+        [nama, alamat, agama],
+         (err, results) => {
+        if (err) throw err;
+        res.status(201).json({ message: 'User Created Successfuly ', id: results.insertId });
+    });
+});
